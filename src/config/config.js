44b57443d1b8c915b06b3d474062f6fa725ed534
KPG-TB <60299538+KPGTB@users.noreply.github.com>
2021-02-27 20:33:22 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "NzcwMzc3MzgyODYzMDQ0Njg4.X5cr2A.4U8iUlr9bYqATODDxr9796lGt88",
+    token: "",
     prefix: "!",
     footer: "UnrealBOT © 2020 | Innowacyjny, wielofunkcyjny bot",
     cmdCooldown: 5,
